cc1d80b94f0396b91daa4a9dbf3d64adf3e95293
GabrielLR <95001103+Laixsent@users.noreply.github.com>
2023-11-27 00:24:34 -0600

    FixCors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-// src/app.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Request, Response, NextFunction } from 'express';
+
+// Utiliza require para cargar cors
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
+  // Configuración de CORS
+  app.use(cors()); // Opciones de CORS se pueden pasar como argumento a cors()
+
+  // Middleware para registrar las solicitudes entrantes
   app.use((req: Request, res: Response, next: NextFunction) => {
     console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
     next();
