1e207f9a69514a7c69edb8a6ee131f4696341300
Daniel Salazar <dsalazar@grupoinndex.com.mx>
2023-12-08 00:11:00 -0600

    Back productos

--- src/entities/productos/controllers/productos.controller.ts
+++ src/entities/productos/controllers/productos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ProductosDao } from '../dao/productos.dao';
 
@@ -26,6 +26,97 @@ export class ProductosDaoController {
     } catch (error) {
       console.error('Error en el controlador:', error);
       res.status(500).send({ message: 'Error en el servidor' });
+    }
+  }
+
+  @Post('insertarProducto')
+  async insertarProducto(@Req() req: Request, @Res() res: Response){
+    try {
+      console.log(req.body)
+      let response;
+      const daoResponse = await ProductosDao.insertarProducto(req.body.nombre, req.body.imagen);
+
+      if(daoResponse != 0){
+        response = {
+          estatus: 1,
+          mensaje: "Producto registrado"
+        }
+      }else{
+        response = {
+          estatus:0,
+          mensaje: "Error al insertar el producto"
+        }
+      }
+
+      res.send(response);
+
+    } catch (error) {
+      console.log('Error en el controlador: ', error);
+      res.status(500).send({message: 'Error en el servidor'});
+    }
+  }
+
+  @Put('actualizarProducto')
+  async actualizarProducto(@Req() req: Request, @Res() res: Response){
+    try {
+      let response;
+      const daoResponse = await ProductosDao.actualizarProducto(req.body.id, req.body.nombre, req.body.imagen);
+
+      if(daoResponse != 0){
+        response = {
+          estatus: 1,
+          mensaje: "Producto modificado"
+        }
+      }else{
+        response = {
+          estatus:0,
+          mensaje: "Error al modificar el producto"
+        }
+      }
+
+      res.send(response);
+
+    } catch (error) {
+      console.log('Error en el controlador: ', error);
+      res.status(500).send({message: 'Error en el servidor'});
+    }
+  }
+
+  @Delete('eliminarProducto')
+  async eliminarProducto(@Req() req: Request, @Res() res: Response){
+    try {
+      let response;
+      const daoResponse = await ProductosDao.eliminarProducto(req.body.id);
+
+      if(daoResponse != 0){
+        response = {
+          estatus: 1,
+          mensaje: "Producto eliminado"
+        }
+      }else{
+        response = {
+          estatus:0,
+          mensaje: "Error al eliminar el producto"
+        }
+      }
+
+      res.send(response);
+    } catch (error) {
+      console.log('Error en el controlador: ', error);
+      res.status(500).send({message: 'Error en el servidor'});
+    }
+  }
+
+  @Get('ventas')
+  async getObtenerVentas(@Req() req: Request, @Res() res: Response){
+    try {
+      const daoResponse = await ProductosDao.obtenerVentas();
+
+      res.send(daoResponse);
+
+    } catch (error) {
+      console.log('Error en el controlador: ', error);
+      res.status(500).send({message: 'Error en el servidor'});
     }
   }
 
--- src/entities/productos/dao/productos.dao.ts
+++ src/entities/productos/dao/productos.dao.ts
@@ -43,6 +43,82 @@ export class ProductosDao {
         console.error('Error en MovimientosDao:', error);
         throw error;
     }
+  }
 
+  static async insertarProducto(nombre: string, imagen: string){
+    let sql : string;
+
+    try {
+      
+      sql = `INSERT INTO don_galleto.productos(nombre_producto, imagen) VALUES (?,?);`;
+
+      const values = [nombre, imagen];
+
+      const result : any = DatabaseService.executeQuery(sql, values);
+
+      return result.affectedRows;
+
+    } catch (error) {
+      console.log(`Error al insertar dao: `, error);
+      throw error;      
+    }
+  }
+
+  static async actualizarProducto(id:string, nombre:string, imagen:string){
+    let sql:string;
+    let values:any = [];
+    try {
+      
+      if(imagen != '' || imagen != null){
+        sql = `UPDATE don_galleto.productos SET nombre_producto = ?, imagen = ? WHERE id_producto = ?`;
+        values = [nombre, imagen, id];
+      }else{
+        sql = `UPDATE don_galleto.productos SET nombre_producto = ? WHERE id_producto = ?`;
+        values = [nombre, id];
+      }
+
+      const result : any = DatabaseService.executeQuery(sql, values);
+
+      return result.affectedRows;
+
+    } catch (error) {
+      console.log(`Error al actualizar dao: `, error);
+      throw error;
+    }
+  }
+
+  static async eliminarProducto(id: number){
+    let sql:string;
+
+    try {
+      
+      sql = `DELETE FROM don_galleto.productos WHERE id_producto = ?`;
+
+      const values = [id];
+
+      const result : any = await DatabaseService.executeQuery(sql, values);
+      
+      return result.affectedRows;
+
+    } catch (error) {
+      console.log(`Error al insertar dao: `, error);
+      throw error;      
+    }
+  }
+
+  static async obtenerVentas(){
+    let sql: string; 
+    try {
+      sql = `SELECT v.id, v.id_producto, p.nombre_producto, p.imagen, v.id_unidad, uv.nombre_unidad, v.cantidad, v.total, v.fecha_venta FROM don_galleto.ventas AS v
+      JOIN don_galleto.productos AS p ON v.id_producto = p.id_producto
+      JOIN don_galleto.unidades AS uv ON v.id_unidad = uv.id_unidad;`;
+
+      const result = await DatabaseService.executeQuery(sql);
+
+      return result;
+    } catch (error) {
+      console.log("Error el insertar dao: ", error);
+      throw error;
+    }
   }
 }
